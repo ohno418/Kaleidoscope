a7e12072a91ef6e2e79e5d4de255cbd2400f8fb5
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-12-09 16:39:24 +0900

    set up my keymaps

--- examples/Devices/Keyboardio/Atreus/Atreus.ino
+++ examples/Devices/Keyboardio/Atreus/Atreus.ino
@@ -40,7 +40,9 @@
 
 enum {
   MACRO_QWERTY,
-  MACRO_VERSION_INFO
+  MACRO_VERSION_INFO,
+  // Switch IME (ctrl+space).
+  MACRO_SWITCH_IME,
 };
 
 #define Key_Exclamation LSHIFT(Key_1)
@@ -55,50 +57,51 @@ enum {
 
 enum {
   QWERTY,
-  FUN,
-  UPPER
+  SYMS,
+  MISC,
 };
 
 // clang-format off
 KEYMAPS(
   [QWERTY] = KEYMAP_STACKED
   (
-       Key_Q   ,Key_W   ,Key_E       ,Key_R         ,Key_T
-      ,Key_A   ,Key_S   ,Key_D       ,Key_F         ,Key_G
-      ,Key_Z   ,Key_X   ,Key_C       ,Key_V         ,Key_B, Key_Backtick
-      ,Key_Esc ,Key_Tab ,Key_LeftGui ,Key_LeftShift ,Key_Backspace ,Key_LeftControl
+       Key_Q         ,Key_W         ,Key_E              ,Key_R               ,Key_T
+      ,Key_A         ,Key_S         ,Key_D              ,Key_F               ,Key_G
+      ,Key_Z         ,Key_X         ,Key_C              ,Key_V               ,Key_B                   ,Key_Esc
+      ,Key_LeftShift ,MO(MISC)      ,Key_Quote          ,Key_LeftAlt         ,Key_Space               ,Key_LeftControl
 
-                     ,Key_Y     ,Key_U      ,Key_I     ,Key_O      ,Key_P
-                     ,Key_H     ,Key_J      ,Key_K     ,Key_L      ,Key_Semicolon
-       ,Key_Backslash,Key_N     ,Key_M      ,Key_Comma ,Key_Period ,Key_Slash
-       ,Key_LeftAlt  ,Key_Space ,MO(FUN)    ,Key_Minus ,Key_Quote  ,Key_Enter
+                     ,Key_Y         ,Key_U              ,Key_I               ,Key_O                   ,Key_P
+                     ,Key_H         ,Key_J              ,Key_K               ,Key_L                   ,Key_Semicolon
+      ,Key_Backspace ,Key_N         ,Key_M              ,Key_Comma           ,Key_Period              ,Key_Slash
+      ,Key_Tab       ,Key_Enter     ,MO(SYMS)           ,M(MACRO_SWITCH_IME) ,Key_Backtick            ,Key_Backslash
   ),
 
-  [FUN] = KEYMAP_STACKED
+  [SYMS] = KEYMAP_STACKED
   (
-       Key_Exclamation ,Key_At           ,Key_UpArrow   ,Key_Dollar           ,Key_Percent
-      ,Key_LeftParen   ,Key_LeftArrow    ,Key_DownArrow ,Key_RightArrow       ,Key_RightParen
-      ,Key_LeftBracket ,Key_RightBracket ,Key_Hash      ,Key_LeftCurlyBracket ,Key_RightCurlyBracket ,Key_Caret
-      ,TG(UPPER)       ,Key_Insert       ,Key_LeftGui   ,Key_LeftShift        ,Key_Delete         ,Key_LeftControl
+       Key_1         ,Key_2         ,Key_3              ,Key_4               ,Key_5
+      ,___           ,___           ,Key_Equals         ,Key_Minus           ,Key_Quote
+      ,Key_LeftShift ,___           ,LSHIFT(Key_Equals) ,LSHIFT(Key_Minus)   ,LSHIFT(Key_Quote)       ,___
+      ,Key_LeftShift ,___           ,___                ,Key_LeftAlt         ,___                     ,Key_LeftControl
 
-                   ,Key_PageUp   ,Key_7 ,Key_8      ,Key_9 ,Key_Backspace
-                   ,Key_PageDown ,Key_4 ,Key_5      ,Key_6 ,___
-      ,Key_And     ,Key_Star     ,Key_1 ,Key_2      ,Key_3 ,Key_Plus
-      ,Key_LeftAlt ,Key_Space    ,___   ,Key_Period ,Key_0 ,Key_Equals
+                     ,Key_6         ,Key_7              ,Key_8               ,Key_9                   ,Key_0
+                     ,___           ,Key_LeftParen      ,Key_RightParen      ,LSHIFT(Key_LeftBracket) ,LSHIFT(Key_RightBracket)
+      ,___           ,___           ,Key_LeftBracket    ,Key_RightBracket    ,___                     ,___
+      ,Key_Tab       ,Key_Enter     ,___                ,___                 ,___                     ,___
    ),
 
-  [UPPER] = KEYMAP_STACKED
+  [MISC] = KEYMAP_STACKED
   (
-       Key_Insert            ,Key_Home                 ,Key_UpArrow   ,Key_End        ,Key_PageUp
-      ,Key_Delete            ,Key_LeftArrow            ,Key_DownArrow ,Key_RightArrow ,Key_PageDown
-      ,M(MACRO_VERSION_INFO) ,Consumer_VolumeIncrement ,XXX           ,XXX            ,___ ,___
-      ,MoveToLayer(QWERTY)   ,Consumer_VolumeDecrement ,___           ,___            ,___ ,___
+       ___ ,___ ,___ ,___ ,___
+      ,___ ,___ ,___ ,___ ,___
+      ,___ ,___ ,___ ,___ ,___ ,___
+      ,___ ,___ ,___ ,___ ,___ ,___
 
-                ,Key_UpArrow   ,Key_F7              ,Key_F8          ,Key_F9         ,Key_F10
-                ,Key_DownArrow ,Key_F4              ,Key_F5          ,Key_F6         ,Key_F11
-      ,___      ,XXX           ,Key_F1              ,Key_F2          ,Key_F3         ,Key_F12
-      ,___      ,___           ,MoveToLayer(QWERTY) ,Key_PrintScreen ,Key_ScrollLock ,Consumer_PlaySlashPause
-   )
+      // arrows and media keys
+           ,___           ,Key_PageDown             ,Key_PageUp               ,___                                 ,Key_PrintScreen
+           ,Key_LeftArrow ,Key_DownArrow            ,Key_UpArrow              ,Key_RightArrow                      ,___
+      ,___ ,Consumer_Mute ,Consumer_VolumeDecrement ,Consumer_VolumeIncrement ,Consumer_DisplayBrightnessDecrement ,Consumer_DisplayBrightnessIncrement
+      ,___ ,Key_Enter     ,___                      ,___                      ,___                                 ,___
+   ),
 )
 // clang-format on
 
@@ -187,6 +190,8 @@ const macro_t *macroAction(uint8_t macro_id, KeyEvent &event) {
       Macros.type(PSTR("Keyboardio Atreus - Kaleidoscope "));
       Macros.type(PSTR(BUILD_INFORMATION));
       break;
+    case MACRO_SWITCH_IME:
+      return MACRO(D(LeftControl), D(Space), U(Space));
     default:
       break;
     }
@@ -195,6 +200,11 @@ const macro_t *macroAction(uint8_t macro_id, KeyEvent &event) {
 }
 
 void setup() {
+  QUKEYS(
+    kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(2, 0), Key_LeftShift), // Z/Shift
+  )
+  Qukeys.setMaxIntervalForTapRepeat(0);
+
   Kaleidoscope.setup();
   EEPROMKeymap.setup(9);
 
