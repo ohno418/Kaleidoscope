908a71dc24a61cd7c840e8f6425cdb824e6eaf51
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-12-17 01:50:53 +0900

    tweak

--- examples/Devices/Keyboardio/Atreus/Atreus.ino
+++ examples/Devices/Keyboardio/Atreus/Atreus.ino
@@ -68,12 +68,12 @@ KEYMAPS(
        Key_Q         ,Key_W     ,Key_E    ,Key_R       ,Key_T
       ,Key_A         ,Key_S     ,Key_D    ,Key_F       ,Key_G
       ,Key_Z         ,Key_X     ,Key_C    ,Key_V       ,Key_B      ,Key_Esc
-      ,Key_LeftShift ,___       ,MO(MISC) ,Key_LeftAlt ,Key_Space  ,Key_LeftControl
+      ,Key_LeftShift ,MO(MISC)  ,___      ,Key_LeftAlt ,Key_Space  ,Key_LeftControl
 
                      ,Key_Y     ,Key_U    ,Key_I       ,Key_O      ,Key_P
                      ,Key_H     ,Key_J    ,Key_K       ,Key_L      ,Key_Semicolon
       ,Key_Backspace ,Key_N     ,Key_M    ,Key_Comma   ,Key_Period ,Key_Slash
-      ,Key_Tab       ,Key_Enter ,MO(SYMS) ,MO(MISC)    ,___        ,M(MACRO_SWITCH_IME)
+      ,Key_Tab       ,Key_Enter ,MO(SYMS) ,M(MACRO_SWITCH_IME) ,MO(MISC) ,___
   ),
 
   [SYMS] = KEYMAP_STACKED
