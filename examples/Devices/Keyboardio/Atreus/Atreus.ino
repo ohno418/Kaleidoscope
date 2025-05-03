5f21a57d6ba2ab57eb9f4cabe98c671ae5ce484c
Yutaro Ohno <yutaro.ono.418@gmail.com>
2025-05-03 18:02:13 +0900

    tweak

--- examples/Devices/Keyboardio/Atreus/Atreus.ino
+++ examples/Devices/Keyboardio/Atreus/Atreus.ino
@@ -69,8 +69,8 @@ KEYMAPS(
   (
      Key_Q           ,Key_W           ,Key_E           ,Key_R            ,Key_T
     ,Key_A           ,Key_S           ,Key_D           ,Key_F            ,Key_G
-    ,Key_Z           ,Key_X           ,Key_C           ,Key_V            ,Key_B           ,Key_Tab
-    ,Key_Esc         ,Key_Backtick    ,MO(MISC)        ,Key_LeftShift    ,Key_Space       ,Key_LeftControl
+    ,Key_Z           ,Key_X           ,Key_C           ,Key_V            ,Key_B           ,Key_Esc
+    ,Key_Tab         ,Key_Backtick    ,MO(MISC)        ,Key_LeftShift    ,Key_Space       ,Key_LeftControl
 
                      ,Key_Y           ,Key_U           ,Key_I            ,Key_O           ,Key_P
                      ,Key_H           ,Key_J           ,Key_K            ,Key_L           ,Key_Semicolon
@@ -82,13 +82,13 @@ KEYMAPS(
   (
      Key_1           ,Key_2           ,Key_3           ,Key_4            ,Key_5
     ,LSHIFT(Key_1)   ,LSHIFT(Key_2)   ,Key_Equals      ,Key_Minus        ,Key_Quote
-    ,LSHIFT(Key_3)   ,LSHIFT(Key_4)   ,Key_Plus        ,Key_Underscore   ,Key_DoubleQuote ,Key_Tab
-    ,Key_Esc         ,___             ,___             ,Key_LeftShift    ,Key_Space       ,Key_LeftControl
+    ,LSHIFT(Key_3)   ,LSHIFT(Key_4)   ,Key_Plus        ,Key_Underscore   ,Key_DoubleQuote ,___
+    ,Key_Esc         ,Key_Tab         ,___             ,Key_LeftShift    ,Key_Space       ,Key_LeftControl
 
                      ,Key_6           ,Key_7           ,Key_8            ,Key_9           ,Key_0
                      ,LSHIFT(Key_8)   ,Key_LeftParen   ,Key_RightParen   ,Key_LeftCurly   ,Key_RightCurly
-    ,Key_Backspace   ,LSHIFT(Key_7)   ,Key_LeftBracket ,Key_RightBracket ,LSHIFT(Key_5)   ,LSHIFT(Key_6)
-    ,Key_RightAlt    ,Key_Enter       ,___             ,___              ,___             ,Key_RightShift
+    ,___             ,LSHIFT(Key_7)   ,Key_LeftBracket ,Key_RightBracket ,LSHIFT(Key_5)   ,LSHIFT(Key_6)
+    ,Key_RightAlt    ,Key_Enter       ,___             ,___              ,Key_RightShift  ,Key_Backspace
   ),
 
   [MISC] = KEYMAP_STACKED
